84e99e93f86c25d70e6e2469ea611fde1d3c4913
Andreas Garvik <andreas.garvik13@gmail.com>
2020-03-11 16:44:49 +0100

    :sparkles:

---
+++ lib/src/widgets/news_list_tile.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import '../models/item_model.dart';
+import '../blocs/stories_provider.dart';
+import 'loading_container.dart';
+
+class NewsListTile extends StatelessWidget {
+  final int itemId;
+
+  NewsListTile({this.itemId});
+
+  @override
+  Widget build(BuildContext context) {
+    final bloc = StoriesProvider.of(context);
+
+    return StreamBuilder(
+      stream: bloc.items,
+      builder: (context,
+          AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: LoadingContainer(),
+          );
+        }
+        return FutureBuilder(
+          future: snapshot.data[itemId],
+          builder: (context, AsyncSnapshot<ItemModel> itemSnapshot) {
+            if (!itemSnapshot.hasData) {
+              return Center(
+                child: LoadingContainer(),
+              );
+            }
+            return buildTile(itemSnapshot.data);
+          },
+        );
+      },
+    );
+  }
+
+  Widget buildTile(ItemModel item) {
+    return Column(
+      children: [
+        ListTile(
+          title: Text(item.title),
+          subtitle: Text('${item.score} points'),
+          trailing: Column(
+            children: [
+              Icon(Icons.comment),
+              Text('${item.descendants}'),
+            ],
+          ),
+        ),
+        Divider(
+          height: 8.0,
+        ),
+      ],
+    );
+  }
+}
